00eeb4fffe574a8162bb6366d66dc4dbd51863fd
rahil471 <rahilsk47@gmail.com>
2015-08-30 13:33:52 +0530

    Server pagination

--- app/app.js
+++ app/app.js
@@ -1,13 +1,19 @@
 var app = angular.module('angularTable', ['angularUtils.directives.dirPagination']);
 
-app.controller('listdata',function($scope, $http){
-	$scope.users = []; //declare an empty array
-	$http.get("mockJson/mock.json").success(function(response){ 
-		$scope.users = response;  //ajax request to fetch data into $scope.data
-	});
+app.controller('listdata',function($http){
+	var vm = this;
+	vm.users = []; //declare an empty array
+	vm.pageno = 1; // initialize page no to 1
+	vm.total_count = 0;
+	vm.itemsPerPage = 10; //this could be a dynamic value from a drop down
 	
-	$scope.sort = function(keyname){
-		$scope.sortKey = keyname;   //set the sortKey to the param passed
-		$scope.reverse = !$scope.reverse; //if true make it false and vice versa
-	}
+	vm.getData = function(pageno){ // This would fetch the data on page change.
+		//In practice this should be in a factory.
+		vm.users = [];
+		$http.get("http://code.ciphertrick.com/api/list/page/"+vm.itemsPerPage+"/"+pageno).success(function(response){ 
+			vm.users = response.data;  //ajax request to fetch data into vm.data
+			vm.total_count = response.total_count;
+		});
+	};
+	vm.getData(vm.pageno); // Call the function to fetch initial data on page load.
 });
